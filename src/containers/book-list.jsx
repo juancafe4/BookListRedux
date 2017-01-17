a57787a377cce4bf236bd8b5af099858d583fb41
Juan Carlos <juancafe2@gmail.com>
2017-01-17 10:49:57 -0400

    map dispatch to actions

--- src/containers/book-list.jsx
+++ src/containers/book-list.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 // Smart Component 
 class BookList extends Component {
   renderList() {
     return this.props.books.map( book => {
       return (
-        <li key={ book.title } className="list-group-item">
+        <li 
+          key={ book.title } 
+          className="list-group-item"
+          onClick={ () => this.props.selectBook(book)}
+        >
           {book.title}
         </li>
       );
@@ -29,4 +35,17 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+
+// Anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBoo is called the result should be passed
+  // to all our reducers
+
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+// Promote bookList from a component to a container it needs to know
+// about this new dispatch method, selectBook. Make it avaliable 
+// as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
